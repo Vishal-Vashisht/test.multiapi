97bf649a5f5974e98fe0b53eb607b325eb86574f
---list <vashisth671@gmail.com>
2026-04-22 20:55:34 +0530

    api cofig fixed

--- config/config.py
+++ config/config.py
@@ -23,250 +23,235 @@ API_CONFIG = {
     "/api/v1/echo/": {
         "methods": set(("POST",)),
         "is_authenticated": False,
-        "POST_data": {"body": {
-            "data": {"required": True, "type": "object"},
-            "error": {"required": False, "type": "object"},
-        },
-    },
-    "/api/v1/trvlr/": {"methods": set(("GET",)), "is_authenticated": False},
-    "/api/v1/auth/register/usercart/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
         "POST_data": {
             "body": {
-                "name": {"type": "string", "required": True},
-                "email": {"type": "string", "required": True},
-                "gender": {"type": "string", "required": True},
-                "refrence_id": {"type": "integer", "required": False},
-            }
+                "data": {"required": True, "type": "object"},
+                "error": {"required": False, "type": "object"},
+            },
         },
-    },
-    "/api/v1/auth/register/userapp/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-        "POST_data": {
-            "body": {
-                "name": {"type": "string", "required": True},
-                "email": {"type": "string", "required": True},
-                "gender": {"type": "string", "required": True},
-                "refrence_id": {"type": "integer", "required": False},
-            }
-        },
-    },
-    "/api/v1/auth/insert/user-app-post/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-        "POST_data": {
-            "body": {
-                "name": {"type": "string", "required": True},
-                "email": {"type": "string", "required": True},
-                "gender": {"type": "string", "required": True},
-                "refrence_id": {"type": "integer", "required": False},
-            }
-        },
-    },
-    "/api/v1/auth/parallel/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-        "POST_data": {
-            "body": {
-                "name": {"type": "string", "required": True},
-                "email": {"type": "string", "required": True},
-                "gender": {"type": "string", "required": True},
-                "refrence_id": {"type": "integer", "required": False},
-            }
-        },
-    },
-    "/api/v1/sample/": {
-        "methods": set(("GET",)),
-        "is_authenticated": False,
-    },
-    "/api/v1/auth/user/login/": {
-        "methods": set(("POST",)),
-        "is_authenticated": False,
-        "POST_data": {
-            "body": {
-                "username": {"required": True, "type": "string"},
-                "password": {"required": True, "type": "string"},
-            }
-        },
-    },
-    "/api/v1/tables/async/delete/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-    },
-    "/api/v1/tables/sync/delete/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-    },
-    "/api/v1/tables/task/info/": {
-        "methods": set(("GET",)),
-        "is_authenticated": True,
-        "GET_data": {
-            "query_params": {"process_id": {"type": "string", "required": True}}
-        },
-    },
-    "/api/v1/data/": {
-        "methods": set(("GET", "POST")),
-        "is_authenticated": True,
-        "GET_data": {
-            "query_params": {
-                "page": {"type": "integer", "required": False},
-                "size": {"type": "integer", "required": False},
-            }
-        },
-        "POST_data": {
-            "query_params": {
-                "page": {"type": "integer", "required": False},
-                "size": {"type": "integer", "required": False},
-            }
-        },
-    },
-    "/api/v1/entity/": {
-        "methods": set(("POST", "GET")),
-        "is_authenticated": True,
-        "POST_data": {
-            "body": {
-                "entity_name": {"type": "string", "required": True},
-                "entity_alias": {"type": "string", "required": True},
-                "columns_config": {
-                    "type": "object"
-                },
-                "relations_config": {
-                    "type": "object"
-                },
-            }
-        },
-    },
-    "/api/v1/datatypes/": {
-        "methods": set(("GET",)),
-        "is_authenticated": True,
-        "POST_data": {
-        },
-    },
-    "/api/v1/entity/<int:pk>/": {
-        "methods": set(("GET",)),
-        "is_authenticated": True,
-        "GET_data": {}
-    },
-    "/api/v1/api-config/": {
-        "methods": set(("POST", "GET")),
-        "is_authenticated": True,
-        "POST_data": {
-            "body": {
-                "name": {
-                    "type": "string",
-                    "description": "The name of the API configuration",
-                    "minLength": 1,
-                    "required": True,
-                },
-                "entity": {
-                    "type": "integer",
-                    "description": "The entity ID related to the API",
-                    "required": True,
-                },
-                "route": {
-                    "type": "string",
-                    "description": "The route for the API",
-                    "minLength": 1,
-                    "required": True,
-                    "example": "product-getitems/"
-                },
-                "method": {
-                    "type": "string",
-                    "enum": ["get", "post", "put", "delete"],
-                    "description": "The HTTP method for the API",
-                    "required": True,
-                },
-                "description": {
-                    "type": "string",
-                    "description": "A description of the API",
-                    "minLength": 1,
-                    "required": True,
-                },
-                "response": {
-                    "type": "array",
-                    "items": {"type": "string"},
-                    "description": "List of response fields",
-                    "required": True,
-                },
+        "/api/v1/trvlr/": {"methods": set(("GET",)), "is_authenticated": False},
+        "/api/v1/auth/register/usercart/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+            "POST_data": {
                 "body": {
-                    "type": "object",
-                    "description": "The request body for the API",
-                    "properties": {
-                        "payload_key": {
-                            "type": "object",
-                            "properties": {
-                                "type": {
-                                    "type": "string",
-                                    "required": True
-                                },
-                                "required": {
-                                    "type": "boolean"
-                                }
-                            }
-                        }
-                    }
-                },
-                "query_params": {
-                    "type": "object",
-                    "description": "Query parameters for the API",
-                    "properties": {
-                        "param": {
-                            "type": "object",
-                            "properties": {
-                                "type": {
-                                    "type": "string",
-                                    "required": True
-                                },
-                                "required": {
-                                    "type": "boolean"
-                                }
-                            }
-                        }
-                    }
-                },
-                "is_authenticated": {
-                    "type": "boolean",
-                    "description": "Whether the API requires authentication",
-                    "required": True,
-                },
-            }
+                    "name": {"type": "string", "required": True},
+                    "email": {"type": "string", "required": True},
+                    "gender": {"type": "string", "required": True},
+                    "refrence_id": {"type": "integer", "required": False},
+                }
+            },
         },
-    },
-    "/api/v1/api-config/<int:pk>/": {
-        "methods": set(("GET",)),
-        "is_authenticated": True,
-        "GET_data": {},
-    },
-    "/api/v1/sync/": {
-        "methods": set(("POST",)),
-        "is_authenticated": True,
-        "POST_data": {},
-    },
-    "/api/v1/cloudnary/upload/": {
-        "methods": set(("POST",)),
-        "is_authenticated": False,
-        "POST_data": {},
-    },
-
-    # Frontend
-    "/home/": {"methods": set(("GET",)), "is_authenticated": True, "feroute": True},
-    "/home/data/": {
-        "methods": set(("GET",)),
-        "is_authenticated": True,
-        "feroute": True,
-    },
-    # login screen
-    "/": {"methods": set(("GET",)), "is_authenticated": False, "feroute": True},
-    "/documentation/": {
-        "methods": set(("GET",)),
-        "is_authenticated": False,
-        "feroute": True,
-    },
-    "/doc/": {"methods": set(("GET",)), "is_authenticated": True},
+        "/api/v1/auth/register/userapp/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+            "POST_data": {
+                "body": {
+                    "name": {"type": "string", "required": True},
+                    "email": {"type": "string", "required": True},
+                    "gender": {"type": "string", "required": True},
+                    "refrence_id": {"type": "integer", "required": False},
+                }
+            },
+        },
+        "/api/v1/auth/insert/user-app-post/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+            "POST_data": {
+                "body": {
+                    "name": {"type": "string", "required": True},
+                    "email": {"type": "string", "required": True},
+                    "gender": {"type": "string", "required": True},
+                    "refrence_id": {"type": "integer", "required": False},
+                }
+            },
+        },
+        "/api/v1/auth/parallel/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+            "POST_data": {
+                "body": {
+                    "name": {"type": "string", "required": True},
+                    "email": {"type": "string", "required": True},
+                    "gender": {"type": "string", "required": True},
+                    "refrence_id": {"type": "integer", "required": False},
+                }
+            },
+        },
+        "/api/v1/sample/": {
+            "methods": set(("GET",)),
+            "is_authenticated": False,
+        },
+        "/api/v1/auth/user/login/": {
+            "methods": set(("POST",)),
+            "is_authenticated": False,
+            "POST_data": {
+                "body": {
+                    "username": {"required": True, "type": "string"},
+                    "password": {"required": True, "type": "string"},
+                }
+            },
+        },
+        "/api/v1/tables/async/delete/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+        },
+        "/api/v1/tables/sync/delete/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+        },
+        "/api/v1/tables/task/info/": {
+            "methods": set(("GET",)),
+            "is_authenticated": True,
+            "GET_data": {
+                "query_params": {"process_id": {"type": "string", "required": True}}
+            },
+        },
+        "/api/v1/data/": {
+            "methods": set(("GET", "POST")),
+            "is_authenticated": True,
+            "GET_data": {
+                "query_params": {
+                    "page": {"type": "integer", "required": False},
+                    "size": {"type": "integer", "required": False},
+                }
+            },
+            "POST_data": {
+                "query_params": {
+                    "page": {"type": "integer", "required": False},
+                    "size": {"type": "integer", "required": False},
+                }
+            },
+        },
+        "/api/v1/entity/": {
+            "methods": set(("POST", "GET")),
+            "is_authenticated": True,
+            "POST_data": {
+                "body": {
+                    "entity_name": {"type": "string", "required": True},
+                    "entity_alias": {"type": "string", "required": True},
+                    "columns_config": {"type": "object"},
+                    "relations_config": {"type": "object"},
+                }
+            },
+        },
+        "/api/v1/datatypes/": {
+            "methods": set(("GET",)),
+            "is_authenticated": True,
+            "POST_data": {},
+        },
+        "/api/v1/entity/<int:pk>/": {
+            "methods": set(("GET",)),
+            "is_authenticated": True,
+            "GET_data": {},
+        },
+        "/api/v1/api-config/": {
+            "methods": set(("POST", "GET")),
+            "is_authenticated": True,
+            "POST_data": {
+                "body": {
+                    "name": {
+                        "type": "string",
+                        "description": "The name of the API configuration",
+                        "minLength": 1,
+                        "required": True,
+                    },
+                    "entity": {
+                        "type": "integer",
+                        "description": "The entity ID related to the API",
+                        "required": True,
+                    },
+                    "route": {
+                        "type": "string",
+                        "description": "The route for the API",
+                        "minLength": 1,
+                        "required": True,
+                        "example": "product-getitems/",
+                    },
+                    "method": {
+                        "type": "string",
+                        "enum": ["get", "post", "put", "delete"],
+                        "description": "The HTTP method for the API",
+                        "required": True,
+                    },
+                    "description": {
+                        "type": "string",
+                        "description": "A description of the API",
+                        "minLength": 1,
+                        "required": True,
+                    },
+                    "response": {
+                        "type": "array",
+                        "items": {"type": "string"},
+                        "description": "List of response fields",
+                        "required": True,
+                    },
+                    "body": {
+                        "type": "object",
+                        "description": "The request body for the API",
+                        "properties": {
+                            "payload_key": {
+                                "type": "object",
+                                "properties": {
+                                    "type": {"type": "string", "required": True},
+                                    "required": {"type": "boolean"},
+                                },
+                            }
+                        },
+                    },
+                    "query_params": {
+                        "type": "object",
+                        "description": "Query parameters for the API",
+                        "properties": {
+                            "param": {
+                                "type": "object",
+                                "properties": {
+                                    "type": {"type": "string", "required": True},
+                                    "required": {"type": "boolean"},
+                                },
+                            }
+                        },
+                    },
+                    "is_authenticated": {
+                        "type": "boolean",
+                        "description": "Whether the API requires authentication",
+                        "required": True,
+                    },
+                }
+            },
+        },
+        "/api/v1/api-config/<int:pk>/": {
+            "methods": set(("GET",)),
+            "is_authenticated": True,
+            "GET_data": {},
+        },
+        "/api/v1/sync/": {
+            "methods": set(("POST",)),
+            "is_authenticated": True,
+            "POST_data": {},
+        },
+        "/api/v1/cloudnary/upload/": {
+            "methods": set(("POST",)),
+            "is_authenticated": False,
+            "POST_data": {},
+        },
+        # Frontend
+        "/home/": {"methods": set(("GET",)), "is_authenticated": True, "feroute": True},
+        "/home/data/": {
+            "methods": set(("GET",)),
+            "is_authenticated": True,
+            "feroute": True,
+        },
+        # login screen
+        "/": {"methods": set(("GET",)), "is_authenticated": False, "feroute": True},
+        "/documentation/": {
+            "methods": set(("GET",)),
+            "is_authenticated": False,
+            "feroute": True,
+        },
+        "/doc/": {"methods": set(("GET",)), "is_authenticated": True},
+    }
 }
-
 # if static folder changes define it in this list
 # STATIC_CONFIG = {
 #     "folders": set((
@@ -287,6 +272,5 @@ API_DOC = {
         }
     },
 }
-
 
 DEPRECATED_ENDPOINTS = set(("/api/v1/trvlr/",))
